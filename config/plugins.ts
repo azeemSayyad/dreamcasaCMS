aecfa5f3ad2342d2b9380aaacb3934c48add3d82
sayyad azeem <azeemsayyd1652@gmail.com>
2025-02-21 23:49:46 +0530

    second

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,8 +1,3 @@
-console.log("AWS_BUCKET_NAME:", process.env.AWS_BUCKET_NAME);
-console.log("AWS_REGION:", process.env.AWS_REGION);
-console.log("AWS_ACCESS_KEY_ID:", process.env.AWS_ACCESS_KEY_ID);
-console.log("AWS_SECRET_ACCESS_KEY:", process.env.AWS_SECRET_ACCESS_KEY);
-
 
 module.exports = ({ env }) => ({
   upload: {
